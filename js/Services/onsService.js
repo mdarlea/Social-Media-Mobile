667b152b4b26a1a8077e47bfc8e0f5efcdbc2995
Michelle Darlea <mdarlea@gmail.com>
2015-03-17 10:26:58 -0400

    Removed hard-coded debugging params

--- js/Services/onsService.js
+++ js/Services/onsService.js
@@ -8,9 +8,9 @@
                 if (isAuth) {
                     authService.authorize({
                         provider: "Twitter",
-                        externalUserName: "christianity017",
-                        access_token: "QQXaziIXta0-yOPHxcHWnW57SOGFcTzzrH63MUb6qYgt2NjYLDDCS5Y5FvPxW0Er9ioHRKHaQoPox8KwdyuxkHKS7yQa6w567wVbITO327b8R-jPFEhHzThA2J8BAEm5Xi6jT3DS25oLr_QiGRLkUONbN4qAfl_7RhaC2qBRejy4aZ-SfiA2tL7yL1ycImBfp36HUcI_qEe8n0-vkUtvTcmGc7OHlIO7P5WNsddK8zxDc0a05TmsVXhE59PK4B_0y-Uk1JT2-rigvhQMjhNJFw",
-                        userName: "michelle.darlea@gmail.com"
+                        externalUserName: "",
+                        access_token: "",
+                        userName: "abc@gmail.com"
                     });
                 } else {
                     authService.logOut();
